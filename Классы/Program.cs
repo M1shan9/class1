93c892b06a893dbb2b0fcb1eb151af01f8b8a758
M1shan9 <118421201+M1shan9@users.noreply.github.com>
2022-11-19 03:08:53 +0900

    Initial commit

---
+++ Классы/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Классы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Users user = new Users()
+            {
+                Name = "Михаил",
+                Surname = "Асламов",
+                Login = "admin",
+                Password = "admin",
+            };
+            Console.WriteLine(user.Name);
+            Console.WriteLine("Введите имя");
+            user.Name = Console.ReadLine();
+            Console.WriteLine($"Ваше новое имя - {user.Name}");
+            Console.ReadLine();
+        }
+
+        class Users
+        {
+            public string Name;
+            public string Surname;
+
+            public string Login;
+            public string Password;
+        }
+    }
+}
